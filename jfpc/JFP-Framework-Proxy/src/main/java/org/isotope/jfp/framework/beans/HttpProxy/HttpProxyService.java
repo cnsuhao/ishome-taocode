e48e9c30601f471a25cf79cbdb8645f921dae905
ishome <ishome@0f64846e-ad2d-472a-bb4f-a9ad5a697426>
2016-04-25 08:47:25 +0000

    IE网络代理服务端定义

---
+++ jfpc/JFP-Framework-Proxy/src/main/java/org/isotope/jfp/framework/beans/HttpProxy/HttpProxyService.java
@@ -0,0 +1,16 @@
+﻿package org.isotope.jfp.framework.beans.HttpProxy;
+
+import org.isotope.jfp.framework.support.MyServiceSupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/** Http代理地址*/
+@Service("HttpProxyService")
+public class HttpProxyService extends MyServiceSupport {
+	protected static final Logger logger = LoggerFactory.getLogger(HttpProxyService.class);
+
+	public HttpProxyDao getDao() {
+		return getMySqlSession().getMapper(HttpProxyDao.class);
+	}
+}
