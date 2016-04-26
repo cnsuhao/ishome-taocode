b9abba0241942981010b425d504e602ee19d6337
ishome <ishome@0f64846e-ad2d-472a-bb4f-a9ad5a697426>
2016-04-26 02:25:03 +0000

    常量整理

---
+++ jfpc/JFP-Framework-ElasticSearch/src/main/java/org/isotope/jfp/framework/search/ISSentenceConstants.java
@@ -0,0 +1,34 @@
+package org.isotope.jfp.framework.search;
+
+import org.isotope.jfp.framework.constants.ISFrameworkConstants;
+
+public interface ISSentenceConstants extends ISFrameworkConstants{
+
+	public final static String VERSION = "341:";
+	
+	/**
+	 * 最后更新时间
+	 */
+	public final static String SENTENCE_UTD = "SENTENCE"+VERSION+"UTD:";
+	/**
+	 * 更新语句
+	 */
+	public final static String SENTENCE_UPD = "SENTENCE"+VERSION+"UPD:";
+	/**
+	 * 创建语句
+	 */
+	public final static String SENTENCE_CRT = "SENTENCE"+VERSION+"CRT:";
+	/**
+	 * 查询语句
+	 */
+	public final static String SENTENCE_SCH = "SENTENCE"+VERSION+"SCH:";
+	/**
+	 * 最后同步日期
+	 */
+	public final static String SENTENCE_SYN = "SENTENCE"+VERSION+"SYN:";
+	
+	/**
+	 * 工商URL地址
+	 */
+	public static final String COMPANY_URL = "COMPANY"+VERSION+"CAPURL:";
+}
