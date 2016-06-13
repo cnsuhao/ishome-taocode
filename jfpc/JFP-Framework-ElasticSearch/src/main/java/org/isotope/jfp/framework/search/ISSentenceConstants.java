8c38ba93d59c9a6b08c15f16bd4c03690313ec66
ishome <ishome@0f64846e-ad2d-472a-bb4f-a9ad5a697426>
2016-06-13 17:39:10 +0000

    配置文件修正

--- jfpc/JFP-Framework-ElasticSearch/src/main/java/org/isotope/jfp/framework/search/ISSentenceConstants.java
+++ jfpc/JFP-Framework-ElasticSearch/src/main/java/org/isotope/jfp/framework/search/ISSentenceConstants.java
@@ -3,37 +3,35 @@ package org.isotope.jfp.framework.search;
 import org.isotope.jfp.framework.constants.ISFrameworkConstants;
 
 public interface ISSentenceConstants extends ISFrameworkConstants{
-
-	public final static String VERSION = "312:";
 	
 	/**
 	 * 最后更新时间
 	 */
-	public final static String SENTENCE_UTD = "SENTENCE"+VERSION+"UTD:";
+	public final static String SENTENCE_UTD = "SENTENCE:UTD:";
 	/**
 	 * 更新语句
 	 */
-	public final static String SENTENCE_UPD = "SENTENCE"+VERSION+"UPD:";
+	public final static String SENTENCE_UPD = "SENTENCE:UPD:";
 	/**
 	 * 创建语句
 	 */
-	public final static String SENTENCE_CRT = "SENTENCE"+VERSION+"CRT:";
+	public final static String SENTENCE_CRT = "SENTENCE:CRT:";
 	/**
 	 * 查询语句
 	 */
-	public final static String SENTENCE_SCH = "SENTENCE"+VERSION+"SCH:";
+	public final static String SENTENCE_SCH = "SENTENCE:SCH:";
 	/**
 	 * 最后同步日期
 	 */
-	public final static String SENTENCE_SYN = "SENTENCE"+VERSION+"SYN:";
+	public final static String SENTENCE_SYN = "SENTENCE:SYN:";
 	
 	/**
 	 * 工商URL地址
 	 */
-	public static final String INDEX_URL = "INDEX"+VERSION+"CAPURL:";
+	public static final String INDEX_URL = "INDEX:CAPURL:";
 	
 	/**
 	 * 索引数据删除
 	 */
-	public static final String INDEX_DEL = "INDEX"+VERSION+"DEL:";
+	public static final String INDEX_DEL = "INDEX:DEL:";
 }
