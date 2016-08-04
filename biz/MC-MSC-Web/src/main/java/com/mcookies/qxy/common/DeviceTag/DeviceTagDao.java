765cb0bbff3a78c23a66471531f86b3348b14cb7
tangmuming <tangmuming@0f64846e-ad2d-472a-bb4f-a9ad5a697426>
2016-08-04 08:30:16 +0000

    模块开发

--- biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/DeviceTag/DeviceTagDao.java
+++ biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/DeviceTag/DeviceTagDao.java
@@ -1,8 +1,13 @@
 package com.mcookies.qxy.common.DeviceTag;
 
+import java.util.List;
+import java.util.Map;
+
 import org.isotope.jfp.framework.support.IDatabaseSupport;
 
 /** 设备标签表 */
 public interface DeviceTagDao extends IDatabaseSupport {
+	
+	List<DeviceTagPVO> doSelectDevice(Map<String,Object> param);
 
 }
