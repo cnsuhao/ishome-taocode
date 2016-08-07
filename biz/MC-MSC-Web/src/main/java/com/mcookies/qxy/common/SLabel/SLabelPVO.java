1d594fabe18c7f9385478d7a71ada5070dd0415e
tangmuming <tangmuming@0f64846e-ad2d-472a-bb4f-a9ad5a697426>
2016-08-07 05:46:01 +0000

    模块开发

--- biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/SLabel/SLabelPVO.java
+++ biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/SLabel/SLabelPVO.java
@@ -5,4 +5,29 @@ import javax.inject.Named;
 /** 教工标签表*/
 public class SLabelPVO extends SLabelDBO
 {
+	/**
+	 * 联系电话
+	 */
+	private String phone = null;
+
+	/**
+	 * 邮箱
+	 */
+	private String email = null;
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	} 
 }
--- biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/SLabelTeacher/SLabelTeacherDao.java
+++ biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/SLabelTeacher/SLabelTeacherDao.java
@@ -1,8 +1,9 @@
 package com.mcookies.qxy.common.SLabelTeacher;
 
 import org.isotope.jfp.framework.support.IDatabaseSupport;
+import org.isotope.jfp.framework.support.MyDataBaseObjectSupport;
 
 /** 教工标签关联表 */
 public interface SLabelTeacherDao extends IDatabaseSupport {
-
+	int doDeleteBytid(MyDataBaseObjectSupport paramBean);
 }
