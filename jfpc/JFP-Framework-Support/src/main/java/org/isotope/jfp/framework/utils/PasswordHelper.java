4349c4100104db36402e89e0cd73694b29d53d81
ishome <ishome@0f64846e-ad2d-472a-bb4f-a9ad5a697426>
2016-08-09 07:02:24 +0000

    框架优化整合

--- jfpc/JFP-Framework-Support/src/main/java/org/isotope/jfp/framework/utils/PasswordHelper.java
+++ jfpc/JFP-Framework-Support/src/main/java/org/isotope/jfp/framework/utils/PasswordHelper.java
@@ -2,20 +2,28 @@ package org.isotope.jfp.framework.utils;
 
 import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 
-/** 
- * 图片压缩工具类 提供的方法中可以设定生成的 缩略图片的大小尺寸、压缩尺寸的比例、图片的质量等 
- *  
- * @date 2012/11/14 
- * @versions 1.0 
- */  
+/**
+ * 密码md5加密
+ * 
+ * @date 2012/11/14
+ * @versions 1.0
+ */
 public class PasswordHelper {
+	public static void main(String[] args) {
+		System.out.println(getPassword("123456"));
+	}
 
 	/**
-	 * @param args
-	 * @return 
+	 * MD5加盐
+	 * 
+	 * @param pass
+	 * @return
 	 */
 	public static String getPassword(String pass) {
-		return new Md5PasswordEncoder().encodePassword(pass, null);
+		String md5 = new Md5PasswordEncoder().encodePassword(pass, null);
+		//System.out.println(md5);
+		md5 = pass.substring(0, 3) + md5.substring(3);
+		return md5;
 	}
 
 }
