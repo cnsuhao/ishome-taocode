33d5b51aea7353f0531361dc9da1bc50e087d9dd
ishome <ishome@0f64846e-ad2d-472a-bb4f-a9ad5a697426>
2016-08-10 04:31:24 +0000

    开发环境配置更新

--- biz/MC-MSC-Web/src/main/java/org/isotope/jfp/framework/filter/CORSFilter.java
+++ biz/MC-MSC-Web/src/main/java/org/isotope/jfp/framework/filter/CORSFilter.java
@@ -22,6 +22,7 @@ public class CORSFilter extends OncePerRequestFilter {
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		response.setHeader("Content-Type", "application/json");
 		filterChain.doFilter(request, response);
 		
 	}
