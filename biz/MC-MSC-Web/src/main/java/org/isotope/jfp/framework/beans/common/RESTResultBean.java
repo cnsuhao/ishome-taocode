a586c78893e93c70777c9c3c7b206ffa77dd0a62
ishome <ishome@0f64846e-ad2d-472a-bb4f-a9ad5a697426>
2016-08-11 02:44:03 +0000

    短信模块独立

--- biz/MC-MSC-Web/src/main/java/org/isotope/jfp/framework/beans/common/RESTResultBean.java
+++ biz/MC-MSC-Web/src/main/java/org/isotope/jfp/framework/beans/common/RESTResultBean.java
@@ -1,15 +1,9 @@
 package org.isotope.jfp.framework.beans.common;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.inject.Named;
 
 import org.isotope.jfp.framework.beans.user.TokenBean;
 import org.isotope.jfp.framework.constants.ISFrameworkConstants;
-
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 
 /**
  * 接口数据返回主体
@@ -61,29 +55,21 @@ public class RESTResultBean extends TokenBean implements ISFrameworkConstants {
 		this.data = data;
 	}
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// RESTResultBean深层对象解析示例
-		// 定义个深层对象
-		RESTResultBean rs = new RESTResultBean();
-		List<RESTResultBean> ls = new ArrayList<RESTResultBean>();
-		ls.add(new RESTResultBean());
-		rs.setData(ls);
-		// rs.setResult(new RESTResultBean());
-		// 转换成文本
-		String json = JSONObject.toJSONString(rs);
-		// 开始解析
-		JSONObject jsons = JSONObject.parseObject(json);
-		{
-			// 直接获得深层对象List
-			JSONArray ja = (JSONArray) jsons.get("result");
-			// System.out.println(JSONArray.parseArray(JSONArray.toJSONString(rs.getResult()),
-			// RESTResultBean.class));
-			// 直接解析数据项目得到一个数据
-			// System.out.println(JSONObject.toJavaObject((JSONObject)
-			// ja.get(0), RESTResultBean.class));
-		}
+	////////////////////////////////////////////////////////////////////////////////
+	protected String code = null;
+	protected String message = null;
+	protected Object result = null;
+
+	public void setCode(String resultCode) {
+		this.code = resultCode;
 	}
+
+	public void setMessage(String resultMsg) {
+		this.message = resultMsg;
+	}
+
+	public void setResult(Object result) {
+		this.result = result;
+	}
+
 }
