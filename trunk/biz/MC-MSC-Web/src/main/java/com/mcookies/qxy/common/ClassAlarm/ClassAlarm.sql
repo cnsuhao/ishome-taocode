389864fcce141b3bf909b24f9892784d8f54c532
ishome <ishome@0f64846e-ad2d-472a-bb4f-a9ad5a697426>
2016-08-12 07:30:20 +0000

    数据结构变更，增加dis数据主键

--- trunk/biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/ClassAlarm/ClassAlarm.sql
+++ trunk/biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/ClassAlarm/ClassAlarm.sql
@@ -1,6 +1,7 @@
 CREATE TABLE class_alarm
 (
-    cid BIGINT(12) unsigned NOT NULL AUTO_INCREMENT COMMENT '班级id',
+    did BIGINT(18) unsigned NOT NULL AUTO_INCREMENT COMMENT '数据id',
+    cid BIGINT(12) NOT NULL COMMENT '班级id',
     sid BIGINT(12) NOT NULL COMMENT '学校id',
     alarmrule_id BIGINT(12) NOT NULL COMMENT '规则id',
     is_use TINYINT(1) COMMENT '是否启用',
@@ -8,6 +9,6 @@ CREATE TABLE class_alarm
     creator BIGINT(12) COMMENT '创建者',
     update_time DATETIME COMMENT '更新时间',
     updator BIGINT(12) COMMENT '最后更新者',
-PRIMARY KEY (cid)
+PRIMARY KEY (did)
 ) COMMENT '班级报警规则关联表'
 ;
--- trunk/biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/ClassAlarm/ClassAlarmDBO.java
+++ trunk/biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/ClassAlarm/ClassAlarmDBO.java
@@ -7,100 +7,119 @@ import org.isotope.jfp.framework.support.MyDataBaseObjectSupport;
 @Named
 /** 班级报警规则关联表 */
 public class ClassAlarmDBO extends MyDataBaseObjectSupport {
-	/**
-	 * 班级id
-	 */
-	private Long cid = null;
-
-	/**
-	 * 学校id
-	 */
-	private Long sid = null;
-
-	/**
-	 * 规则id
-	 */
-	private Long alarmruleId = null;
-
-	/**
-	 * 是否启用
-	 */
-	private Integer isUse = null;
-
-	/**
-	 * 获取班级id
-	 *
-	 * @return Cid 班级id
-	 */
-	public Long getCid() {
-		return this.cid;
-	}
-
-	/**
-	 * 获取学校id
-	 *
-	 * @return Sid 学校id
-	 */
-	public Long getSid() {
-		return this.sid;
-	}
-
-	/**
-	 * 获取规则id
-	 *
-	 * @return Alarmrule_id 规则id
-	 */
-	public Long getAlarmruleId() {
-		return this.alarmruleId;
-	}
-
-	/**
-	 * 获取是否启用
-	 *
-	 * @return Is_use 是否启用
-	 */
-	public Integer getIsUse() {
-		return this.isUse;
-	}
-
-	/**
-	 * 设置班级id
-	 *
-	 * @param Cid
-	 *            班级id
-	 */
-	public void setCid(Long cid) {
-		this.cid = cid;
-	}
-
-	/**
-	 * 设置学校id
-	 *
-	 * @param Sid
-	 *            学校id
-	 */
-	public void setSid(Long sid) {
-		this.sid = sid;
-	}
-
-	/**
-	 * 设置规则id
-	 *
-	 * @param Alarmrule_id
-	 *            规则id
-	 */
-	public void setAlarmruleId(Long alarmruleid) {
-		this.alarmruleId = alarmruleid;
-	}
-
-	/**
-	 * 设置是否启用
-	 *
-	 * @param Is_use
-	 *            是否启用
-	 */
-	public void setIsUse(Integer isuse) {
-		this.isUse = isuse;
-	}
-
+	/** 
+     * 数据id
+     */
+    private Long did = null;
+ 
+    /** 
+     * 班级id
+     */
+    private Long cid = null;
+ 
+    /** 
+     * 学校id
+     */
+    private Long sid = null;
+ 
+    /** 
+     * 规则id
+     */
+    private Long alarmruleId = null;
+ 
+    /** 
+     * 是否启用
+     */
+    private Integer isUse = null;
+  
+    /** 
+     * 获取数据id
+     *
+     * @return Did 数据id
+     */
+    public Long getDid() {
+        return this.did;
+    }
+ 
+    /** 
+     * 获取班级id
+     *
+     * @return Cid 班级id
+     */
+    public Long getCid() {
+        return this.cid;
+    }
+ 
+    /** 
+     * 获取学校id
+     *
+     * @return Sid 学校id
+     */
+    public Long getSid() {
+        return this.sid;
+    }
+ 
+    /** 
+     * 获取规则id
+     *
+     * @return Alarmrule_id 规则id
+     */
+    public Long getAlarmruleId() {
+        return this.alarmruleId;
+    }
+ 
+    /** 
+     * 获取是否启用
+     *
+     * @return Is_use 是否启用
+     */
+    public Integer getIsUse() {
+        return this.isUse;
+    }
+ 
+    /** 
+     * 设置数据id
+     *
+     * @param Did 数据id
+     */
+    public void setDid(Long did) {
+        this.did = did;
+    }
+ 
+    /** 
+     * 设置班级id
+     *
+     * @param Cid 班级id
+     */
+    public void setCid(Long cid) {
+        this.cid = cid;
+    }
+ 
+    /** 
+     * 设置学校id
+     *
+     * @param Sid 学校id
+     */
+    public void setSid(Long sid) {
+        this.sid = sid;
+    }
+ 
+    /** 
+     * 设置规则id
+     *
+     * @param Alarmrule_id 规则id
+     */
+    public void setAlarmruleId(Long alarmruleid) {
+        this.alarmruleId = alarmruleid;
+    }
+ 
+    /** 
+     * 设置是否启用
+     *
+     * @param Is_use 是否启用
+     */
+    public void setIsUse(Integer isuse) {
+        this.isUse = isuse;
+    }
+ 
 }
