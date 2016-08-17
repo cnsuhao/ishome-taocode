da392a7515ebe43353f595e61b490d011ad6d798
ishome <ishome@0f64846e-ad2d-472a-bb4f-a9ad5a697426>
2016-08-17 07:01:28 +0000

    工具类优化

--- trunk/jfpc/JFP-Framework-Support/src/main/java/org/isotope/jfp/framework/utils/FilePathHelper.java
+++ trunk/jfpc/JFP-Framework-Support/src/main/java/org/isotope/jfp/framework/utils/FilePathHelper.java
@@ -16,84 +16,84 @@ import org.isotope.jfp.framework.constants.ISFrameworkConstants;
  * @version 0.2 2014/10/8
  * 
  */
-//TODO
+// TODO
 public class FilePathHelper implements ISFrameworkConstants {
 
 	/**
 	 * 图片种类
 	 */
-	public static String IMAG_TYPE = "";	
-	
-	public static String CONTENT_TYPE_FILE = "/fileContentType.xml";
-	
-	private static ArrayList<ArrayList<String>> FileTypes = new ArrayList<ArrayList<String>>();
-	static {
-		String[][] IMAGE = { 
-		{ ".jpg", ".0", "image/jpeg","0" }, 
-		{ ".png", ".1", "image/png","0" }, 
-		{ ".gif", ".2", "image/gif","0" }, 
-		{ ".bmp", ".3", "application/x-bmp","0" }, 
-		{ ".jpeg", ".4", "image/jpeg","0" }, 
-		{ ".html", ".5", "text/html","1" },
-		{ ".pdf", ".6", "application/pdf","1" }, 
-		{ ".htm", ".7", "text/html","1" },
-		{ ".doc", ".8", "application/msword","1" } 
-		};
-		
-			StringBuffer its = new StringBuffer();
+	public static String IMAG_TYPE = "";
 
-			for(String[] item :IMAGE){
-				ArrayList<String> is = new ArrayList<String>();
-				is.add(item[0]);is.add(item[1]);is.add(item[2]);
-				if(ZERO.equals(item[3]))
-					its.append("||"+item[0]);
-				FileTypes.add(is);
-			}
-			
-			IMAG_TYPE = its.toString();
-			
-//		try {
-//			Properties props = new Properties();
-//			InputStream in = FilePathHelper.class.getResourceAsStream(CONTENT_TYPE_FILE);
-//			props.load(in);
-//			machinecode = props.getProperty(MACHINE_CODE);
-//		} catch (Exception e) {
-//			//System.out.println("It's not exit File (fileContentType.xml)!");
-//		}
+	public static String CONTENT_TYPE_FILE = "/fileContentType.xml";
+
+	private static ArrayList<ArrayList<String>> FileTypes = new ArrayList<ArrayList<String>>();
+
+	static {
+		String[][] IMAGE = { { ".jpg", ".0", "image/jpeg", "0" },
+				{ ".png", ".1", "image/png", "0" }, 
+				{ ".gif", ".2", "image/gif", "0" }, 
+				{ ".bmp", ".3", "application/x-bmp", "0" }, 
+				{ ".jpeg", ".4", "image/jpeg", "0" }, 
+				{ ".html", ".5", "text/html", "1" }, 
+				{ ".pdf", ".6", "application/pdf", "1" }, 
+				{ ".htm", ".7", "text/html", "1" }, 
+				{ ".doc", ".8", "application/msword", "1" } };
+
+		StringBuffer its = new StringBuffer();
+
+		for (String[] item : IMAGE) {
+			ArrayList<String> is = new ArrayList<String>();
+			is.add(item[0]);
+			is.add(item[1]);
+			is.add(item[2]);
+			if (ZERO.equals(item[3]))
+				its.append("||" + item[0]);
+			FileTypes.add(is);
+		}
+
+		IMAG_TYPE = its.toString();
+
+		// try {
+		// Properties props = new Properties();
+		// InputStream in =
+		// FilePathHelper.class.getResourceAsStream(CONTENT_TYPE_FILE);
+		// props.load(in);
+		// machinecode = props.getProperty(MACHINE_CODE);
+		// } catch (Exception e) {
+		// //System.out.println("It's not exit File (fileContentType.xml)!");
+		// }
 	}
-	
-//	public final static String[][] IMAGE = { 
-//		{ ".jpg", ".0", "image/jpeg" }, 
-//		{ ".png", ".1", "image/png" }, 
-//		{ ".gif", ".2", "image/gif" }, 
-//		{ ".bmp", ".3", "application/x-bmp" }, 
-//		{ ".jpeg", ".4", "image/jpeg" }, 
-//		{ ".html", ".5", "text/html" },
-//		{ ".pdf", ".6", "application/pdf" }, 
-//		{ ".htm", ".7", "text/html" } 
-//		};
+
+	// public final static String[][] IMAGE = {
+	// { ".jpg", ".0", "image/jpeg" },
+	// { ".png", ".1", "image/png" },
+	// { ".gif", ".2", "image/gif" },
+	// { ".bmp", ".3", "application/x-bmp" },
+	// { ".jpeg", ".4", "image/jpeg" },
+	// { ".html", ".5", "text/html" },
+	// { ".pdf", ".6", "application/pdf" },
+	// { ".htm", ".7", "text/html" }
+	// };
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		String[] s = makeFilePath("spook.jpg");
-		//System.out.println("=="+(s[0]));
-		//System.out.println("=="+(s[1]));
-		//System.out.println("=="+(s[2]));
-		//System.out.println("=="+(s[3]));
-		//System.out.println("==111=="+getFilePath("11451642260080981322426024_3_7_6_1_0_0_0_._6",true)[0]);
-		//System.out.println("==111=="+getFilePath("11451642260080981322426024_3_7_6_1_0_0_0_._6",true)[1]);
-		//System.out.println("==111=="+getFilePath("11451642260080981322426024_3_7_6_1_0_0_0_._6",true)[2]);
-		//System.out.println("==111=="+getFilePath("11451642260080981322426024_3_7_6_1_0_0_0_._6",true)[3]);
-		//System.out.println("==111=="+getFilePath("11451642260080981322426024_3_7_6_1_0_0_0_._6",true)[4]);
+		// System.out.println("=="+(s[0]));
+		// System.out.println("=="+(s[1]));
+		// System.out.println("=="+(s[2]));
+		// System.out.println("=="+(s[3]));
+		// System.out.println("==111=="+getFilePath("11451642260080981322426024_3_7_6_1_0_0_0_._6",true)[0]);
+		// System.out.println("==111=="+getFilePath("11451642260080981322426024_3_7_6_1_0_0_0_._6",true)[1]);
+		// System.out.println("==111=="+getFilePath("11451642260080981322426024_3_7_6_1_0_0_0_._6",true)[2]);
+		// System.out.println("==111=="+getFilePath("11451642260080981322426024_3_7_6_1_0_0_0_._6",true)[3]);
+		// System.out.println("==111=="+getFilePath("11451642260080981322426024_3_7_6_1_0_0_0_._6",true)[4]);
 	}
-	
+
 	/**
 	 * 根据当前时间获得一个存储路径
-	 * @return String[0] 文件全路径
-	 * 			String[1] 文件路径
-	 * 			String[2] 文件名称（全）
-	 * 			String[3] 文件扩展名
+	 * 
+	 * @return String[0] 文件全路径 String[1] 文件路径 String[2] 文件名称（全） String[3] 文件扩展名
 	 */
 	public static String[] getFilePath(String filePathId, boolean change) {
 		String[] filePath = new String[5];
@@ -102,18 +102,18 @@ public class FilePathHelper implements ISFrameworkConstants {
 		String ext = StringUtils.trimToEmpty(StringUtils.substring(filePath[2], StringUtils.lastIndexOf(filePath[2], DOT)));
 		if (change == true) {
 			for (ArrayList<String> item : FileTypes) {
-				if (item.get(1).equals(ext)){
+				if (item.get(1).equals(ext)) {
 					filePath[2] = filePath[2].replace(item.get(1), item.get(0));
 					filePath[3] = item.get(0);
 					filePath[4] = item.get(2);
 					break;
 				}
 			}
-		}else{
+		} else {
 			filePath[3] = ext;
 			filePath[4] = "";
 		}
-		filePath[0] =  filePath[1] + filePath[2];
+		filePath[0] = filePath[1] + filePath[2];
 		return filePath;
 	}
 
@@ -212,6 +212,7 @@ public class FilePathHelper implements ISFrameworkConstants {
 		}
 		return ext;
 	}
+
 	/**
 	 * 混淆生成图片识别ID，保存于数据库内
 	 * 
