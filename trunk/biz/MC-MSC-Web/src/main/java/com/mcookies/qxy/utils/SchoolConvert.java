2c43c2e45c726bd52f550671a1354942d16da12b
ishome <ishome@0f64846e-ad2d-472a-bb4f-a9ad5a697426>
2016-08-17 21:55:19 +0000

    学校ID拦截器完善

--- trunk/biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/utils/SchoolConvert.java
+++ trunk/biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/utils/SchoolConvert.java
@@ -2,17 +2,39 @@ package com.mcookies.qxy.utils;
 
 import org.apache.commons.beanutils.BeanUtils;
 import org.isotope.jfp.framework.beans.common.FrameworkDataBean;
+import org.isotope.jfp.framework.beans.user.UserBean;
+import org.isotope.jfp.framework.cache.session.SessionHelper;
+import org.isotope.jfp.framework.constants.ISFrameworkConstants;
 import org.isotope.jfp.framework.mybatis.IParameterConvertSupport;
 
-public class SchoolConvert implements IParameterConvertSupport {
+/**
+ * 学校ID拦截器
+ * @author 001745
+ *
+ */
+public class SchoolConvert implements ISFrameworkConstants, IParameterConvertSupport {
+	private UserBean loginer;
 
-	public String getSchoolId() {
-		return "123456789";
+	public UserBean getLoginer() {
+		if (loginer == null)
+			loginer = SessionHelper.getSessionAttribute();
+		return loginer;
 	}
 
+	public Long getSchoolId() {
+		return getLoginer().getSchoolId();
+	}
+
+	/**
+	 * 当puk=1的时候停止拦截
+	 */
 	@Override
 	public void convert(FrameworkDataBean paramBean) throws Exception {
-		BeanUtils.setProperty(paramBean, "sid", getSchoolId());
+		if (ONE.equals(paramBean.getPuk())) {
+
+		} else {
+			BeanUtils.setProperty(paramBean, "sid", getSchoolId());
+		}
 
 	}
 
