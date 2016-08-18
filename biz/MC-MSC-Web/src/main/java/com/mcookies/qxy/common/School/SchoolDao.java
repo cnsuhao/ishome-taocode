396c29382f1a51a12ef1664935dce11d2cf7e74b
linyh <linyh@0f64846e-ad2d-472a-bb4f-a9ad5a697426>
2016-08-18 14:05:48 +0000

    1. login.4 学校管理相关

--- biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/School/SchoolDao.java
+++ biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/School/SchoolDao.java
@@ -1,8 +1,12 @@
 package com.mcookies.qxy.common.School;
 
+import java.util.List;
+
 import org.isotope.jfp.framework.support.IDatabaseSupport;
 
 /** 学校表 */
 public interface SchoolDao extends IDatabaseSupport {
+
+	List<SchoolDBO> doSelectByIsUse(SchoolDBO school);
 
 }
