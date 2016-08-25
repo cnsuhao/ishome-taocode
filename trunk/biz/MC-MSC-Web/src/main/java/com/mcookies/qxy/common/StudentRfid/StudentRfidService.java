4d9110258ee411c025c2687711276262376dae9f
ishome <ishome@0f64846e-ad2d-472a-bb4f-a9ad5a697426>
2016-08-25 03:56:12 +0000

    设备对应学生学期班级学校信息查询 CardInfoPVO doReadCardInfoByRfid(StudentRfidDBO sr);

--- trunk/biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/StudentRfid/StudentRfidService.java
+++ trunk/biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/StudentRfid/StudentRfidService.java
@@ -4,6 +4,8 @@ import org.isotope.jfp.framework.beans.page.PageVOSupport;
 import org.isotope.jfp.framework.support.sync.MyServiceSupport;
 import org.springframework.stereotype.Service;
 
+import com.mcookies.qxy.biz.device.CardInfoPVO;
+
 /** 学生一卡通关联表 */
 @Service
 public class StudentRfidService extends MyServiceSupport {
@@ -19,5 +21,13 @@ public class StudentRfidService extends MyServiceSupport {
 	public PageVOSupport doSelectPageStudentByrfid(PageVOSupport formParamPageModel) {
 		formParamPageModel.setPageListData(getDao().doSelectPageStudentByrfid(formParamPageModel));
 		return formParamPageModel;
+	}
+	/**
+	 * 设备对应学生学期班级学校信息查询
+	 * @param sr
+	 * @return
+	 */
+	public CardInfoPVO doReadCardInfoByRfid(StudentRfidDBO sr) {
+		return getDao().doReadCardInfoByRfid(sr);
 	}
 }
