fa1e220ea71a55bb4904087cf92da0c575427558
ishome <ishome@0f64846e-ad2d-472a-bb4f-a9ad5a697426>
2016-08-26 09:45:39 +0000

    用户Token可以使用时间定义来源于系统配置spring-system.xml

--- jfpc/JFP-Common-Web/src/main/java/org/isotope/jfp/framework/utils/token/UserCacheHelper.java
+++ jfpc/JFP-Common-Web/src/main/java/org/isotope/jfp/framework/utils/token/UserCacheHelper.java
@@ -2,6 +2,8 @@ package org.isotope.jfp.framework.utils.token;
 
 import org.isotope.jfp.framework.beans.user.UserBean;
 import org.isotope.jfp.framework.cache.ICacheService;
+import org.isotope.jfp.framework.constants.ISFrameworkConstants;
+import org.isotope.jfp.framework.support.MySystemConfig;
 import org.isotope.jfp.framework.utils.BeanFactoryHelper;
 import org.isotope.jfp.framework.utils.EmptyHelper;
 
@@ -13,8 +15,8 @@ import com.alibaba.fastjson.JSON;
  * @author 001745
  *
  */
-public class UserCacheHelper {
-	
+public class UserCacheHelper implements ISFrameworkConstants {
+
 	public static void main(String[] args) throws Exception {
 		UserBean user = new UserBean();
 		user.setSchoolId(987654321L);
@@ -22,6 +24,7 @@ public class UserCacheHelper {
 		user.setUserType("0");
 		System.out.println(JSON.toJSONString(user));
 	}
+
 	/**
 	 * 获得登录用户信息
 	 * 
@@ -29,7 +32,7 @@ public class UserCacheHelper {
 	 * @return
 	 */
 	public static UserBean checkUser(String token) {
-		try{
+		try {
 			if (EmptyHelper.isEmpty(token))
 				return null;
 			ICacheService myCache = BeanFactoryHelper.getBean("myCache");
@@ -37,11 +40,11 @@ public class UserCacheHelper {
 			String obj = (String) myCache.getObject(token, false);
 			if (EmptyHelper.isEmpty(obj))
 				return null;
-	
+
 			UserBean loginer = JSON.parseObject(obj, UserBean.class);
 			myCache.init();
 			return loginer;
-		}catch(Exception e){
+		} catch (Exception e) {
 			return null;
 		}
 	}
@@ -55,16 +58,18 @@ public class UserCacheHelper {
 		if (EmptyHelper.isEmpty(loginer))
 			return;
 		ICacheService myCache = BeanFactoryHelper.getBean("myCache");
+		MySystemConfig mySystemConfig = BeanFactoryHelper.getBean("mySystemConfig");
+
 		myCache.selectDB(3);
-		myCache.putObject(loginer.getToken(), JSON.toJSONString(loginer), 7200, false);
+		myCache.putObject(loginer.getToken(), JSON.toJSONString(loginer), Integer.parseInt(mySystemConfig.getConfig(Token_Can_Use_Hour)) * 3600, false);
 		myCache.init();
 	}
 
 	public static UserBean removeUser(String token) {
 		if (EmptyHelper.isEmpty(token))
 			return null;
-		
-		try{
+
+		try {
 			if (EmptyHelper.isEmpty(token))
 				return null;
 			ICacheService myCache = BeanFactoryHelper.getBean("myCache");
@@ -72,11 +77,11 @@ public class UserCacheHelper {
 			String obj = (String) myCache.deleteObject(token, false);
 			if (EmptyHelper.isEmpty(obj))
 				return null;
-	
+
 			UserBean loginer = JSON.parseObject(obj, UserBean.class);
 			myCache.init();
 			return loginer;
-		}catch(Exception e){
+		} catch (Exception e) {
 			return null;
 		}
 	}
