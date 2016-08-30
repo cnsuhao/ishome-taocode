17d98b20a5f6c44f1e22c4d897a9754c8d40d85f
zhiyi <zhiyi@0f64846e-ad2d-472a-bb4f-a9ad5a697426>
2016-08-30 12:07:27 +0000

    8.1.3 安全日志查询接口，已修改

--- trunk/biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/biz/schoolsafety/SecurityLogController.java
+++ trunk/biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/biz/schoolsafety/SecurityLogController.java
@@ -20,8 +20,10 @@ import com.mcookies.qxy.common.LogSecurity.LogSecurityPVO;
 import com.mcookies.qxy.common.LogSecurity.LogSecurityService;
 import com.mcookies.qxy.common.UStudent.UStudentDBO;
 import com.mcookies.qxy.common.UStudent.UStudentService;
+
 /**
  * 安全日志
+ * 
  * @author macBookTang
  *
  */
@@ -32,16 +34,18 @@ public class SecurityLogController extends MyControllerSupport {
 	@Resource
 	protected ClassStudentService ClassStudentService_;
 	@Resource
-	protected LogSecurityService LogSecurityService_;	
+	protected LogSecurityService LogSecurityService_;
+
 	/**
 	 * 学生列表查询接口
+	 * 
 	 * @param token
 	 * @param cid
 	 * @return
 	 */
 	@RequestMapping(value = "/student/namelist", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
 	@ResponseBody
-	public RESTResultBean stundentNamelistGET(String token,Long cid) {
+	public RESTResultBean stundentNamelistGET(String token, Long cid) {
 		RESTResultBean result = new RESTResultBean();
 		try {
 			if (doCheckToken(token) == false) {
@@ -49,7 +53,7 @@ public class SecurityLogController extends MyControllerSupport {
 			}
 			ClassStudentDBO param = new ClassStudentDBO();
 			param.setCid(cid);
-			List<UStudentDBO> rlist =(List<UStudentDBO>)UStudentService_.doSelectListBycid(param);
+			List<UStudentDBO> rlist = (List<UStudentDBO>) UStudentService_.doSelectListBycid(param);
 			JSONObject data = new JSONObject();
 			data.put("count", rlist.size());
 			data.put("student", rlist);
@@ -62,9 +66,9 @@ public class SecurityLogController extends MyControllerSupport {
 		return result;
 	}
 
-	
 	/**
 	 * 安全日志查询接口
+	 * 
 	 * @param token
 	 * @param cid
 	 * @param studentId
@@ -76,7 +80,7 @@ public class SecurityLogController extends MyControllerSupport {
 	 */
 	@RequestMapping(value = "/securitylog", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
 	@ResponseBody
-	public RESTResultBean securitylogGET(String token,Long cid,Long[] studentId,Long deviceId,String time,Integer page,Integer size) {
+	public RESTResultBean securitylogGET(String token, Long termId, Long cid, Long[] studentId, Long deviceId, String time, Integer page, Integer size) {
 		RESTResultBean result = new RESTResultBean();
 		try {
 			if (doCheckToken(token) == false) {
@@ -88,31 +92,46 @@ public class SecurityLogController extends MyControllerSupport {
 			if (page == null || page == 0) {
 				page = 1;
 			}
-			List<PageVOSupport> plist= new ArrayList<PageVOSupport>();
-			for(Long id :studentId){
-			
+
 			LogSecurityPVO pvo = new LogSecurityPVO();
 			pvo.setCid(cid);
 			pvo.setDeviceId(deviceId);
-			pvo.setStudentId(id);
-			if(time!=null&&!"".equals(time)){
+			if(termId!=null){
+				pvo.setTermId(termId);				
+			}
+			if (time != null && !"".equals(time)) {
 				String[] times = time.split("|");
 				pvo.setStartTime(times[0]);
 				pvo.setEndTime(times[2]);
 			}
-			pageModel.config();
-			pageModel.setPageCurrent(page);
-			pageModel.setPageLimit(size);
-			pageModel.setFormParamBean(pvo);
-			PageVOSupport logSecurity=LogSecurityService_.doSelectPageSecurityLog(pageModel);
-			
-			plist.add(logSecurity);
-			}
 			JSONObject data = new JSONObject();
 			data.put("page", page);
 			data.put("size", size);
-			data.put("count", pageModel.getResultCount());
-			data.put("securitylog", plist);
+			
+			if (studentId!=null) {
+				for (Long id : studentId) {
+					pvo.setStudentId(id);
+					pageModel.config();
+					pageModel.setPageCurrent(page);
+					pageModel.setPageLimit(size);
+					pageModel.setFormParamBean(pvo);
+					LogSecurityService_.doSelectPageSecurityLog(pageModel);
+					data.put("count", pageModel.getResultCount());
+					data.put("securitylog",pageModel.getPageListData());
+					
+				}
+								
+
+			} else {
+				pageModel.config();
+				pageModel.setPageCurrent(page);
+				pageModel.setPageLimit(size);
+				pageModel.setFormParamBean(pvo);
+				LogSecurityService_.doSelectPageSecurityLog(pageModel);
+				data.put("count", pageModel.getResultCount());
+				data.put("securitylog",pageModel.getPageListData());
+			}
+			
 			result.setData(data);
 		} catch (Exception e) {
 			result.setInfo("访问失败");
