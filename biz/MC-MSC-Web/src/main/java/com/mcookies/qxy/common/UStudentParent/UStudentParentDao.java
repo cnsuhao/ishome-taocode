177b59b6b68828cdec900ac9e2373d92660de09a
james_tong <james_tong@0f64846e-ad2d-472a-bb4f-a9ad5a697426>
2016-09-06 02:38:38 +0000

    增加家长角色登录接口修改

--- biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/UStudentParent/UStudentParentDao.java
+++ biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/UStudentParent/UStudentParentDao.java
@@ -6,6 +6,9 @@ import org.isotope.jfp.framework.beans.common.FrameworkDataBean;
 import org.isotope.jfp.framework.beans.page.PageVOSupport;
 import org.isotope.jfp.framework.support.IDatabaseSupport;
 
+import com.mcookies.qxy.common.Class.ClassPVO;
+import com.mcookies.qxy.common.ClassStudent.ClassStudentPVO;
+
 /** 学生家长关联表 */
 public interface UStudentParentDao extends IDatabaseSupport {
 
@@ -14,5 +17,8 @@ public interface UStudentParentDao extends IDatabaseSupport {
 	void updateIsDefaultFalse(UStudentParentDBO sp);
 
 	List<? extends FrameworkDataBean> doSelectPageByTermIdAndCidAndStudentId(PageVOSupport pageModel);
-
+	
+	List<UStudentParentPVO> doFindByParentId(UStudentParentPVO uStudentParentPVO);
+	
+	UStudentParentDBO doReadByParentId(UStudentParentDBO uStudentParentDBO);
 }
