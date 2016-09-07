8e07d31684605a2e28ec85aa615f05c3a6c682be
zhiyi <zhiyi@0f64846e-ad2d-472a-bb4f-a9ad5a697426>
2016-09-07 10:27:23 +0000

    新闻栏目查询接口，已修改

--- biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/NewsColumn/NewsColumnDao.java
+++ biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/NewsColumn/NewsColumnDao.java
@@ -1,7 +1,11 @@
 package com.mcookies.qxy.common.NewsColumn;
+import java.util.List;
+
 import org.isotope.jfp.framework.support.IDatabaseSupport;
 
 /** 栏目表 */
 public interface NewsColumnDao extends IDatabaseSupport {
+	
+	List<NewsColumnDBO> doSelectWithParent(NewsColumnDBO newsCol);
 
 }
