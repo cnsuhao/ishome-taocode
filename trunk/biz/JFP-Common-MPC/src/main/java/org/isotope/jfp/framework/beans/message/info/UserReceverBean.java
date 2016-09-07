8f86415bf4bd0abf8846120948aac6ea80c1c917
ishome <ishome@0f64846e-ad2d-472a-bb4f-a9ad5a697426>
2016-09-07 08:31:30 +0000

    消息中心

---
+++ trunk/biz/JFP-Common-MPC/src/main/java/org/isotope/jfp/framework/beans/message/info/UserReceverBean.java
@@ -0,0 +1,15 @@
+package org.isotope.jfp.framework.beans.message.info;
+
+import org.isotope.jfp.framework.beans.common.FrameworkDataBean;
+
+/**
+ * 消息接收者
+ * 
+ * @author spookfcy
+ * @since 3.3.1
+ * @version 3.3.1.20160825
+ *
+ */
+public class UserReceverBean extends FrameworkDataBean {
+
+}
