8867504e20881f94937d428fdb2fbf5d3be0c558
zhiyi <zhiyi@0f64846e-ad2d-472a-bb4f-a9ad5a697426>
2016-09-07 10:27:23 +0000

    新闻栏目查询接口，已修改

--- trunk/biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/NewsColumn/NewsColumnService.java
+++ trunk/biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/NewsColumn/NewsColumnService.java
@@ -1,5 +1,7 @@
 package com.mcookies.qxy.common.NewsColumn;
 
+import java.util.List;
+
 import org.isotope.jfp.framework.support.sync.MyServiceSupport;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +11,10 @@ public class NewsColumnService extends MyServiceSupport {
 
 	public NewsColumnDao getDao() {
 		return getMySqlSession().getMapper(NewsColumnDao.class);
+	}
+	
+	public List<NewsColumnDBO> doSelectWithParent(NewsColumnDBO newsCol) {
+		return getDao().doSelectWithParent(newsCol);
 	}
 
 }
