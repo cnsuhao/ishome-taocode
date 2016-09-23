dcdcf16e7627cac8322b7e571e757cb9ed30fcbb
zhiyi <zhiyi@0f64846e-ad2d-472a-bb4f-a9ad5a697426>
2016-09-23 12:47:20 +0000

    班级作业题目模块，数据上传

---
+++ trunk/biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/TaskQuestion/TaskQuestionDao.java
@@ -0,0 +1,8 @@
+package com.mcookies.qxy.common.TaskQuestion;
+
+import org.isotope.jfp.framework.support.IDatabaseSupport;
+
+/** 作业题目表*/
+public interface TaskQuestionDao extends IDatabaseSupport{
+
+}
---
+++ trunk/biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/TaskQuestion/TaskQuestionPVO.java
@@ -0,0 +1,8 @@
+﻿package com.mcookies.qxy.common.TaskQuestion;
+import javax.inject.Named;
+ 
+@Named
+/** 作业题目表*/
+public class TaskQuestionPVO extends TaskQuestionDBO
+{
+}
