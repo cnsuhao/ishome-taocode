c79146fbf795bed13e8a82e2b7d56cebb322a2fb
ishome <ishome@0f64846e-ad2d-472a-bb4f-a9ad5a697426>
2016-09-26 04:28:00 +0000

    企业微信号框架提交

--- trunk/biz/JFP-MPC-WeiXin/src/main/java/org/isotope/jfp/mpc/weixin/service/MyWeixinMessageService.java
+++ trunk/biz/JFP-MPC-WeiXin/src/main/java/org/isotope/jfp/mpc/weixin/service/MyWeixinMessageService.java
@@ -2,44 +2,126 @@ package org.isotope.jfp.mpc.weixin.service;
 
 import javax.annotation.Resource;
 
-import org.isotope.jfp.common.weixin.WeixinService;
+import org.isotope.jfp.framework.beans.message.MessageInfoBean;
+import org.isotope.jfp.framework.common.message.UserMessageSendService;
 import org.isotope.jfp.framework.constants.ISFrameworkConstants;
+import org.isotope.jfp.framework.constants.pub.ISPushConstant.MessageType;
+import org.isotope.jfp.framework.utils.BeanFactoryHelper;
+import org.isotope.jfp.mpc.weixin.beans.WeiXinCompanyDeptReceverBean;
+import org.isotope.jfp.mpc.weixin.beans.WeiXinCompanySenderBean;
+import org.isotope.jfp.mpc.weixin.beans.WeiXinCompanyTagReceverBean;
 import org.isotope.jfp.mpc.weixin.beans.WeiXinMessageValueBean;
+import org.isotope.jfp.mpc.weixin.beans.WeiXinUserReceverBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 /**
- * 微信数据管理服务<br>
- * 消息信息
+ * 发送微信消息
  * 
  * @author spookfcy
  * @since 3.3.1
  * @version 3.3.1.20160825
- * 
+ * @see <UserMessageSendService>
  */
 @Service("MyWeixinMessageService")
 public class MyWeixinMessageService implements ISFrameworkConstants {
 
 	protected Logger logger = LoggerFactory.getLogger(this.getClass());
-	@Resource
-	WeixinService WeixinService_;
-	@Resource
-	WeixinService TXWeixinService_;
 
-	public String sendToCompanyId(String companyId, WeiXinMessageValueBean message) {
-		// TODO Auto-generated method stub
-		return null;
+	@Resource
+	UserMessageSendService umss;	
+	
+	MessageInfoBean message;
+
+	public MyWeixinMessageService() {
+		message = new MessageInfoBean();
+		message.setMessgeType(MessageType.WeiXin);
 	}
 
-	public String sendToCompanyIdGroupId(String companyId, String groupId, WeiXinMessageValueBean message) {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 * 企业信息发送给自身
+	 * 
+	 * @param companyId 企业ID
+	 * @param message
+	 * @return
+	 */
+	public String sendToCompanyId(String companyId, WeiXinMessageValueBean weixinMessage) {
+		MyWeixinCompanyService mycs = BeanFactoryHelper.getBean("MyWeixinCompanyService");
+		{
+			WeiXinCompanySenderBean sender = mycs.loadWeiXinCompanySenderBean(companyId);
+			if (sender == null)
+				return "9090";
+			message.setSender(sender);
+		}
+		message.setMessage(weixinMessage);
+		{
+			WeiXinCompanyTagReceverBean recever = mycs.loadWeiXinCompanyTagReceverBean(companyId);
+			if (recever == null)
+				return "9010";
+			message.setRecever(recever);
+		}
+
+		umss.send(message);
+		return ZERO;
 	}
 
-	public String sendToCompanyIdUserId(String companyId, String userId, WeiXinMessageValueBean message) {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 * 企业信息发送给部门
+	 * 
+	 * @param companyId 企业ID
+	 * @param groupId 用户组ID
+	 * @param message 
+	 * @return
+	 */
+	public String sendToCompanyIdGroupId(String companyId, String groupId, WeiXinMessageValueBean weixinMessage) {
+		MyWeixinCompanyService mycs = BeanFactoryHelper.getBean("MyWeixinCompanyService");
+		{
+			WeiXinCompanySenderBean sender = mycs.loadWeiXinCompanySenderBean(companyId);
+			if (sender == null)
+				return "9090";
+			message.setSender(sender);
+		}
+		message.setMessage(weixinMessage);
+		MyWeixinDeptService myds = BeanFactoryHelper.getBean("MyWeixinDeptService");
+		{
+			WeiXinCompanyDeptReceverBean recever = myds.loadWeiXinCompanyDeptReceverBean(groupId);
+			if (recever == null)
+				return "9020";
+			message.setRecever(recever);
+		}
+
+		umss.send(message);
+		return ZERO;
+	}
+
+	/**
+	 * 企业信息发送给个人
+	 * 
+	 * @param companyId 企业ID
+	 * @param userId 个人ID
+	 * @param message
+	 * @return
+	 */
+	public String sendToCompanyIdUserId(String companyId, String userId, WeiXinMessageValueBean weixinMessage) {
+		MyWeixinCompanyService mycs = BeanFactoryHelper.getBean("MyWeixinCompanyService");
+		{
+			WeiXinCompanySenderBean sender = mycs.loadWeiXinCompanySenderBean(companyId);
+			if (sender == null)
+				return "9090";
+			message.setSender(sender);
+		}
+		message.setMessage(weixinMessage);
+		MyWeixinUserService myds = BeanFactoryHelper.getBean("MyWeixinUserService");
+		{
+			WeiXinUserReceverBean recever = myds.loadWeiXinUserReceverBean(userId);
+			if (recever == null)
+				return "9030";
+			message.setRecever(recever);
+		}
+
+		umss.send(message);
+		return ZERO;
 	}
 
 }
