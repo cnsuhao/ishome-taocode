79fec36490e2118d0477985c02c26df5e902a017
james_tong <james_tong@0f64846e-ad2d-472a-bb4f-a9ad5a697426>
2016-09-29 02:44:46 +0000

    德育班级管理common

---
+++ trunk/biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/MecItem/MecItemDao.java
@@ -0,0 +1,8 @@
+package com.mcookies.qxy.common.MecItem;
+
+import org.isotope.jfp.framework.support.IDatabaseSupport;
+
+/** 德育班级项目表*/
+public interface MecItemDao extends IDatabaseSupport{
+
+}
---
+++ trunk/biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/MecItem/MecItemPVO.java
@@ -0,0 +1,9 @@
+package com.mcookies.qxy.common.MecItem;
+
+import javax.inject.Named;
+ 
+@Named
+/** 德育班级项目表*/
+public class MecItemPVO extends MecItemDBO{
+	
+}
