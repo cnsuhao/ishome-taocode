b2ab219270f3bc2bc4738fd94d1682a10ab80402
zhiyi <zhiyi@0f64846e-ad2d-472a-bb4f-a9ad5a697426>
2016-09-30 03:15:54 +0000

    设备分组数据，更新

--- biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/DeviceGroup/DeviceGroupService.java
+++ biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/DeviceGroup/DeviceGroupService.java
@@ -1,5 +1,9 @@
 package com.mcookies.qxy.common.DeviceGroup;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
 import org.isotope.jfp.framework.support.sync.MyServiceSupport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +15,10 @@ public class DeviceGroupService extends MyServiceSupport {
 
     public DeviceGroupDao getDao(){
         return getMySqlSession().getMapper(DeviceGroupDao.class);
+    }
+    
+    public List<DeviceGroupPVO> doSelectDeviceGroup(Map<String,Object> param){
+    	return getDao().doSelectDeviceGroup(param);
     }
 
 }
