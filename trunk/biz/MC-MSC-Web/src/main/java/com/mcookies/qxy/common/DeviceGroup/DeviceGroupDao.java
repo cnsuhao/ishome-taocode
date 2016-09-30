0259a287252caabd22ba76c0e9fa30e912196238
zhiyi <zhiyi@0f64846e-ad2d-472a-bb4f-a9ad5a697426>
2016-09-30 03:15:54 +0000

    设备分组数据，更新

--- trunk/biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/DeviceGroup/DeviceGroupDao.java
+++ trunk/biz/MC-MSC-Web/src/main/java/com/mcookies/qxy/common/DeviceGroup/DeviceGroupDao.java
@@ -1,6 +1,11 @@
 package com.mcookies.qxy.common.DeviceGroup;
 
+import java.util.List;
+import java.util.Map;
+
 import org.isotope.jfp.framework.support.IDatabaseSupport;
 public interface DeviceGroupDao extends IDatabaseSupport{
+	
+	List<DeviceGroupPVO> doSelectDeviceGroup(Map<String,Object> param);
 
 }
