c9c8b46867c634359ed180cfef82f2a992bbff07
ishome <ishome@0f64846e-ad2d-472a-bb4f-a9ad5a697426>
2016-12-14 02:04:44 +0000

    社会游戏

--- game/Boxy/JPG-Boxy-Server/src/main/java/org/isotope/boxy/IGameAction.java
+++
@@ -1,8 +0,0 @@
-package org.isotope.boxy;
-
-public interface IGameAction {
-	/**
-	 * 运行游戏指令
-	 */
-	boolean doGameAction() throws Exception;
-}
--- game/Boxy/JPG-Boxy-Server/src/main/java/org/isotope/boxy/common/entity/CommonEntityDao.java
+++
@@ -1,8 +0,0 @@
-package org.isotope.boxy.common.entity;
-
-import org.isotope.jfp.framework.support.IDatabaseSupport;
-
-/** 数据存储*/
-public interface CommonEntityDao extends IDatabaseSupport{
-
-}
